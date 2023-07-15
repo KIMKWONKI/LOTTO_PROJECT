998cb60111eb805b72dfbd5c069d9a46f981c3e4
TK <rlarnjsrl@gmail.com>
2023-07-16 02:49:26 +0900

    셋팅

--- tk/src/main/java/com/tk/controller/Welcome.java
+++ tk/src/main/java/com/tk/controller/Welcome.java
@@ -1,12 +1,16 @@
 package com.tk.controller;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 public class Welcome {
     
     @GetMapping(value = "/welcome")
-    public String index() {
+    public String index(ModelAndView mv) {
+
+        mv.addObject("name" ,"pooney.jsp");
+
         return "welcome";
     }
 }
